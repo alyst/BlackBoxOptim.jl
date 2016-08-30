5c9b79ae7130dcb9360a24ed7181dbaea2dc54d8
Alexey Stukalov <astukalov@gmail.com>
2016-08-30 16:53:02 +0200

    Borg: test Borg with ParallelEvaluator

--- test/test_borg_moea.jl
+++ test/test_borg_moea.jl
@@ -1,11 +1,22 @@
 facts("BorgMOEA") do
     context("Schaffer1") do
-      res = bboptimize(BlackBoxOptim.Schaffer1Family; Method=:borg_moea,
-                       FitnessScheme=ParetoFitnessScheme{2}(is_minimizing=true),
-                       SearchRange=(-10.0, 10.0), NumDimensions=2, ϵ=0.01,
-                       MaxSteps=5000, TraceMode=:silent)
-      @fact BlackBoxOptim.IGD(BlackBoxOptim.Schaffer1Family.opt_value, pareto_frontier(res),
+        res = bboptimize(BlackBoxOptim.Schaffer1Family; Method=:borg_moea,
+                         FitnessScheme=ParetoFitnessScheme{2}(is_minimizing=true),
+                         SearchRange=(-10.0, 10.0), NumDimensions=2, ϵ=0.01,
+                         MaxSteps=5000, TraceMode=:silent)
+        @fact BlackBoxOptim.IGD(BlackBoxOptim.Schaffer1Family.opt_value, pareto_frontier(res),
                 fitness_scheme(res), Val{length(best_candidate(res))}) --> less_than(0.05)
+
+        context("using ParallelEvaluator") do
+            opt = bbsetup(BlackBoxOptim.Schaffer1Family; Method=:borg_moea,
+                          FitnessScheme=ParetoFitnessScheme{2}(is_minimizing=true),
+                          SearchRange=(-10.0, 10.0), NumDimensions=2, ϵ=0.01,
+                          MaxSteps=5000, TraceMode=:verbose, TraceInterval=1.0, Workers=workers())
+            res = bboptimize(opt)
+            @fact isa(BlackBoxOptim.evaluator(lastrun(opt)), BlackBoxOptim.ParallelEvaluator) --> true
+            @fact BlackBoxOptim.IGD(BlackBoxOptim.Schaffer1Family.opt_value, pareto_frontier(res),
+                       fitness_scheme(res), Val{length(best_candidate(res))}) --> less_than(0.05)
+        end
     end
     context("CEC09_UP8") do
       res = bboptimize(BlackBoxOptim.CEC09_Unconstrained_Set[8]; Method=:borg_moea,
